2def349801fae94234bb1865a0af7dd727f988dc
GlebLitvinov <gleb.litvinow@gmail.com>
2015-12-24 11:53:35 +0300

    JsonGetter implemented

--- GoogleNews/app/src/main/java/com/project/googlenews/activities/MainActivity.java
+++ GoogleNews/app/src/main/java/com/project/googlenews/activities/MainActivity.java
@@ -5,11 +5,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.project.googlenews.R;
+import com.project.googlenews.infrastructure.json.JsonGetterAsync;
+
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +36,23 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+            JsonGetterAsync getterAsync = new JsonGetterAsync();
+            getterAsync.execute();
+        try {
+            JSONObject object = getterAsync.get();
+            Log.i("ong",object.toString());
+            View layout = findViewById(R.id.lay_text);
+            TextView textView = (TextView) layout.findViewById(R.id.large_text);
+            textView.setText(object.toString());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
